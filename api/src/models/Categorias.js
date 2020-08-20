27b502381756c1eb8f238e430abf491ca5da6e2b
Juan Bautista Abal <keroxx3@gmail.com>
2020-08-19 21:23:52 -0300

    CAMBIOS GUARDADOS LOCO

---
+++ api/src/models/Categorias.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('categories', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    products: {
+        type: DataTypes.JSON,
+        allowNull: false
+    }
+  });
+};
