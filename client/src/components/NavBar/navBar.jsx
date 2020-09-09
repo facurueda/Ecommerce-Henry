53731380722fd8d4726abbfed0bc4185f409e1bc
Facundo <facu9685@gmail.com>
2020-09-09 15:46:48 -0300

    Modificaciones varias NavBar

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -2,51 +2,103 @@ import React from 'react'
 import Logo from './Images/Logo.png'
 import Cart from './Images/Cart.png'
 import './navBar.css'
+import SearchBar from '../SearchBar/SearchBar'
 
 const NavBar = () => {
     return (
-            <div className='navContainer'>
-                <div className='logoContainer'>
-                    <a href="/">
-                        <img className='imageLogo' src={Logo} alt='Logo'/>
-                    </a>
-                </div>
-                <div className='routerContainer'>
-                    <div className='buttonsContainer'>
-                        <form action="/home">
-                            <button className='buttonHome'>Home</button>
-                        </form>
-                        <form action="/catalogue">
-                            <button className='buttonProducts'>Products</button>
-                        </form>
 
-                    {/* Si esta logeado se muestra el boton My Account */}
+        <div >
+            {window.location.pathname === "/" ? (
 
-                        {/* <form action="/Account">
-                            <button className='buttonProducts'>My Account</button>
-                        </form> */}
-                        
+                // ---------------------------- When the user is in "/" ---------------------------- //
+
+                <div className='navContainer'>
+                    <div className='routerContainer'>
+                        <div className='buttonsContainer'>
+                            <form action="/home">
+                                <button className='buttonHome'>Home</button>
+                            </form>
+                            <form action="/catalogue">
+                                <button className='buttonProducts'>Products</button>
+                            </form>
+
+                            {/* Si esta logeado se muestra el boton My Account */}
+
+                            <form action="/Account">
+                                <button className='buttonProducts'>My Account</button>
+                            </form>
+
+                        </div>
+
+                        {/* Si no esta logeado, se muestra para crear cuenta o logearse */}
+
+                        {/* <div className='registerContainer'>
+                                <div className='signup'>Crear Cuenta</div>
+                                <div className='login'>Iniciar Sesion</div>
+                            </div> */}
+
+
+                        {/* Carrito para cuanto esta logeado */}
+
+                        <div className='cartContainer'>
+                            <a href='/order'>
+                                <img className='buttonCart' src={Cart} alt='Cart' />
+                            </a>
+                            <div className='quantityProducts'>7</div>
+                        </div>
                     </div>
-
-                    {/* Si no esta logeado, se muestra para crear cuenta o logearse */}
-
-                    {/* <div className='registerContainer'>
-                        <div className='signup'>Crear Cuenta</div>
-                        <div className='login'>Iniciar Sesion</div>
-                    </div> */}
-
-
-                    {/* Carrito para cuanto esta logeado */}
-
-                    <div className='cartContainer'>
-                        <a href='/cart'>
-                            <img className='buttonCart' src={Cart} alt='Cart'/>
-                        </a>
-                        <div className='quantityProducts'>7</div>
-                    </div>
                 </div>
-            </div>
-        
+
+            ) : (
+
+                    // ---------------------------- When the user is in on any page ---------------------------- //
+
+                    <div className='navContainer'>
+                        <div className='logoContainer'>
+                            <a href="/">
+                                <img className='imageLogo' src={Logo} alt='Logo' />
+                            </a>
+                        </div>
+                        <div className='routerContainer'>
+                            <div className='buttonsContainer'>
+                                <form action="/home">
+                                    <button className='buttonHome'>Home</button>
+                                </form>
+                                <form action="/catalogue">
+                                    <button className='buttonProducts'>Products</button>
+                                </form>
+
+                                {/* Si esta logeado se muestra el boton My Account */}
+
+                                <form action="/Account">
+                                    <button className='buttonProducts'>My Account</button>
+                                </form>
+                            </div>
+                            <div className='searchBar'>
+                                <SearchBar/>
+                            </div>
+
+                            {/* Si no esta logeado, se muestra para crear cuenta o logearse */}
+
+                            {/* <div className='registerContainer'>
+                                    <div className='signup'>Crear Cuenta</div>
+                                    <div className='login'>Iniciar Sesion</div>
+                                </div> */}
+
+
+                            {/* Carrito para cuanto esta logeado */}
+
+                            <div className='cartContainer'>
+                                <a href='/order'>
+                                    <img className='buttonCart' src={Cart} alt='Cart' />
+                                </a>
+                                <div className='quantityProducts'>7</div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+        </div>
+
     )
 }
 export default NavBar;
