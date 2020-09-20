6eae21ebb45c84012733dde51fd9927304c16ca8
Juan Bautista Abal <keroxx3@gmail.com>
2020-09-20 11:42:26 -0300

    cookie veryfier cedido a back

--- client/src/redux/usersReducer.js
+++ client/src/redux/usersReducer.js
@@ -1,9 +1,9 @@
-import {GET_USER_BY_ID, USER_CREATED, USER_LOGGED_IN, POST_LOGIN, AUTH_FAILED, USER_LOGGED_OUT} from './constants'
+import { GET_USER_BY_ID, USER_CREATED, USER_LOGGED_IN, POST_LOGIN, AUTH_FAILED, USER_LOGGED_OUT } from './constants'
 var initialState = {
     idUser: 0,
     name: 'Guest',
     email: 'no-email',
-    level: 'GUEST'
+    level: 'GUEST',
 }
 
 
@@ -27,11 +27,23 @@ const usersReducer = (state = initialState, action) => {
                 idUser: action.payload.idUser
             }
         case USER_LOGGED_OUT:
-            return state;
+            return {
+                ...state,
+                name: action.payload.name,
+                email: action.payload.email,
+                level: action.payload.level,
+                idUser: action.payload.idUser
+            };
         case POST_LOGIN:
             return state;
         case AUTH_FAILED:
-            return state;
+            return {
+                ...state,
+                name: action.payload.name,
+                email: action.payload.email,
+                level: action.payload.level,
+                idUser: action.payload.idUser
+            };
 
         default:
             return state;
