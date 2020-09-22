a2eb8b2bff15f3044a5a886881d9854478ea1d74
Juan Bautista Abal <keroxx3@gmail.com>
2020-09-21 21:54:16 -0300

    Cambios bien de M***a

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -21,7 +21,7 @@ export const SET_PRODUCT = 'SET_PRODUCT'
 export const GET_ORDER_BY_ID = 'GET_ORDER_BY_ID'
 export const GET_ALL_ORDERS = 'GET_ALL_ORDERS'
 export const UPDATE_ORDER = 'UPDATE_ORDER'
-export const GET_ORDER_QUANTITY = 'GET_ORDER_QUANTITY'
+export const SET_QUANTITY = 'SET_QUANTITY'
 
 export const GET_USER_BY_ID = 'GET_USER_BY_ID'
 export const USER_CREATED = 'USER_CREATED'
--- client/src/redux/ordersActions.js
+++ client/src/redux/ordersActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ORDER_QUANTITY, UPDATE_ORDER, GET_ORDER_BY_ID, GET_ALL_ORDERS, ADD_TO_CART } from "./constants";
+import { SET_QUANTITY, UPDATE_ORDER, GET_ORDER_BY_ID, GET_ALL_ORDERS, ADD_TO_CART } from "./constants";
 const url = "http://localhost:3000/";
 
 export const actionUpdateOrder = (idUser) => {
@@ -10,23 +10,16 @@ export const actionUpdateOrder = (idUser) => {
     }
 }
 export const actionGetOrder = (idUser) => {
-    return async (dispatch) => {
-        await axios.get(url + 'order/' + idUser).then(res => {
-            console.log('res.data: ', res.data)
-            if(res.data.products !== undefined) {
-                dispatch({
-                    type: GET_ORDER_QUANTITY, payload: res.data.products.reduce((acum, product) => {
-                        return acum + product.Inter_Prod_Order.quantity
-                    }, 0)
-                })
-            } else {
-                dispatch({
-                    type: GET_ORDER_QUANTITY, payload: 0
-                })
-            }
-             
+    return (dispatch) => {
+        axios.get(url + 'order/' + idUser).then(res => {
             dispatch({ type: GET_ORDER_BY_ID, payload: res.data })
         })
+    }
+}
+
+export const actionSetQuantity = (quantity) => {
+    return (dispatch) => {
+        dispatch({ type: SET_QUANTITY, payload: quantity })
     }
 }
 
